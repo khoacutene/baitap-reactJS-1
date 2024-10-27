2cb2a25710a956cd2939b1914dad812e43e7f458
khoacutene <dangtienkhoa11192005@gmail.com>
2024-10-27 13:55:17 +0700

    nop bai lan 1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Example1 from "./example-1"
+import Example2 from "./example-2"
+import Rendering_elements from "./rendering-elements"
+import HandlingEvents from "./handlingEvents"
+import State from "./State"
+import CarColor from "./baitap-car-color"
+import ListKey from "./List-key"
+import Communicate from "../public/communicate"
+
+function App() {
+
+  return (
+    <>
+      {/* <Example1/> */}
+      {/* <Example2/> */}
+      {/* <Rendering_elements/> */}
+      {/* <HandlingEvents/> */}
+      {/* <State/> */}
+      <CarColor/>
+      {/* <ListKey/> */}
+      {/* <Communicate/> */}
+    </>
+  )
+}
+
+export default App
